4980c1790b160837d4132c8a0d4b135c17177e4e
madaooftheblues <azlanrasool@gmail.com>
2024-02-20 03:39:51 +0500

    Setup Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,16 @@ module.exports = {
         filename: 'index_bundle.js',
     },
     plugins: [new HtmlWebpackPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 }
